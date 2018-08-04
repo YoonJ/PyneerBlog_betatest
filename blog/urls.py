bb8b5801e47afb50732b689b98547f29ca246f81
YoonJ <oyoons95@gmail.com>
2018-08-04 20:00:23 +0900

    test 01 20:00

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 #blog/urls.py에서 해당 뷰를 가져와서 url에 연결한다.
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.index_page, name='index_page'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name ='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
